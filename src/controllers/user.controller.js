e9e3743fe24548332f726cd741ec9e664e2864d9
Gyelanjjim <songcj92@gmail.com>
2023-02-04 23:22:04 +0900

    [Add] 로그인

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,4 +15,18 @@ const signUp = catchAsync(async (req, res, next) => {
   res.status(201).json({ message: 'userCreated' });
 });
 
-module.exports = { signUp };
+const signIn = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    const err = new Error('KEY_ERROR');
+    err.statusCode = 400;
+    throw err;
+  }
+
+  const accessToken = await userService.signIn(email, password);
+
+  res.status(200).json({ accessToken: accessToken });
+});
+
+module.exports = { signUp, signIn };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 const userDao = require('../models/user.dao');
@@ -20,4 +21,26 @@ const signUp = async (name, email, profileImage, password) => {
   await userDao.signUp(name, email, profileImage, hashedPassword);
 };
 
-module.exports = { signUp };
+const signIn = async (email, password) => {
+  validatePw(password);
+
+  const user = await userDao.getUserByEmail(email);
+
+  if (!user) {
+    const err = new Error('specified user does not exist');
+    err.statusCode = 404;
+    throw err;
+  }
+
+  const result = await bcrypt.compare(password, user.password);
+
+  if (!result) {
+    const err = new Error('invalid password');
+    err.statusCode = 401;
+    throw err;
+  }
+
+  return jwt.sign({ sub: user.id }, process.env.JWT_SECRET);
+};
+
+module.exports = { signUp, signIn };
