8aad5118a380b0baa066b7318a06c0a45057efa3
Gyelanjjim <songcj92@gmail.com>
2023-02-05 00:25:58 +0900

    [Add] userId로 회원정보 조회

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,7 +1,9 @@
 const userRouter = require('express').Router();
 const userController = require('../controllers/user.controller');
+const { checkAuth } = require('../utils/checkAuth');
 
 userRouter.post('/signup', userController.signUp);
 userRouter.post('/signin', userController.signIn);
+userRouter.get('/:userId', checkAuth, userController.getUserByUserId);
 
 module.exports = { userRouter };
